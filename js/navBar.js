fb1eba51895a11665f3b4659372ab70afd0a8b0b
iamepick1 <105526724+iamepick1@users.noreply.github.com>
2026-04-28 12:25:33 -0400

    fixed so much

---
+++ js/navBar.js
@@ -0,0 +1,11 @@
+fetch('../navBar.html')
+            .then(response => {
+                if (!response.ok) throw new Error("Failed to load header");
+                return response.text();
+            })
+            .then(data => {
+                document.getElementById('navBar').innerHTML = data;
+            })
+            .catch(err => console.error(err));
+var conGreet = "Hello what are you looking at!"
+console.log(conGreet)
