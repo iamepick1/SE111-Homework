4071ab032d949b1b5c695a241462e60ee06f1e1f
iamepick1 <105526724+iamepick1@users.noreply.github.com>
2026-04-29 13:16:23 -0400

    calc

---
+++ js/calc.js
@@ -0,0 +1,30 @@
+// function add(a,b){
+//     return a+b
+// }
+// function sub(a,b){
+//     return a-b
+// }
+// function mult(a,b){
+//     return a*b
+// }
+// function div(a,b){
+//     return a/b
+// }
+
+
+var sel = Number(prompt("1, add\n2, subtract\n3, multiply\n4, divide\n\n(put the number of the one you choose)"))
+
+// var n1 = Number(prompt("what is the first number you would like to use"))
+// var n2 = Number(prompt("what is the second number you would like to use"))
+
+// if(sel===1){
+//     alert(add(n1,n2))
+// }else if(sel===2){
+//     alert(sub(n1,n2))
+// }else if(sel===3){
+//     alert(mult(n1,n2))
+// }else if(sel===4){
+//     alert(div(n1,n2))
+// }else{
+//     alert("please put a number 1-4 inclusive")
+// }
