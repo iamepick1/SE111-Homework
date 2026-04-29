4f4f985a64b1fcbf9b68bf457b361f20e8e96b61
iamepick1 <105526724+iamepick1@users.noreply.github.com>
2026-04-29 13:29:18 -0400

    a

--- js/calc.js
+++ js/calc.js
@@ -11,20 +11,21 @@ function div(a,b){
     return a/b
 }
 
+while(0==0){
+    var sel = Number(prompt("1, add\n2, subtract\n3, multiply\n4, divide\n\n(put the number of the one you choose)"))
 
-var sel = Number(prompt("1, add\n2, subtract\n3, multiply\n4, divide\n\n(put the number of the one you choose)"))
+    var n1 = Number(prompt("what is the first number you would like to use"))
+    var n2 = Number(prompt("what is the second number you would like to use"))
 
-var n1 = Number(prompt("what is the first number you would like to use"))
-var n2 = Number(prompt("what is the second number you would like to use"))
-
-if(sel===1){
-    alert(add(n1,n2))
-}else if(sel===2){
-    alert(sub(n1,n2))
-}else if(sel===3){
-    alert(mult(n1,n2))
-}else if(sel===4){
-    alert(div(n1,n2))
-}else{
-    alert("please put a number 1-4 inclusive")
+    if(sel===1){
+        alert(add(n1,n2))
+    }else if(sel===2){
+        alert(sub(n1,n2))
+    }else if(sel===3){
+        alert(mult(n1,n2))
+    }else if(sel===4){
+        alert(div(n1,n2))
+    }else{
+        alert("please put a number 1-4 inclusive")
+    }
 }
--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,4 @@
-fetch('../navBar.html')
+fetch('../../navBar.html')
             .then(response => {
                 if (!response.ok) throw new Error("Failed to load header");
                 return response.text();
